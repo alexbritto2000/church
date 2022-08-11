a2e1a74e151ae1000a1ea2a49c406d9891332402
alexbritto2000 <alex.britto@sabbatech.com>
2022-08-11 10:13:30 +0530

    sfsf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,55 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MinistryComponent } from './pages/ministry/ministry.component';
+import { WomenFellowshipComponent } from './pages/ministry/women-fellowship/women-fellowship.component';
+import { YouthFellowshipComponent } from './pages/ministry/youth-fellowship/youth-fellowship.component';
+import { ChildrenMinistryComponent } from './pages/ministry/children-ministry/children-ministry.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { TimeComponent } from './pages/time/time.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  /* {   path:'', redirectTo:'home',pathMatch:'full' },
+  {   path:'**', redirectTo:'home'  }, */
+  {
+    path: "ministry",
+    component: MinistryComponent
+  },
+  {
+    path: "women_fellowship",
+    component: WomenFellowshipComponent
+  },
+  {
+    path: "you_fellowship",
+    component: YouthFellowshipComponent
+  },
+  {
+    path: "child_fellowship",
+    component: ChildrenMinistryComponent
+  },
+  {
+    path: "history",
+    component: HistoryComponent
+  },
+  {
+    path: "time",
+    component: TimeComponent
+  },
+  {
+    path: "gallery",
+    component: GalleryComponent
+  },
+  {
+    path: "contact",
+    component: ContactComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,53 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MinistryComponent } from './pages/ministry/ministry.component';
+import { WomenFellowshipComponent } from './pages/ministry/women-fellowship/women-fellowship.component';
+import { YouthFellowshipComponent } from './pages/ministry/youth-fellowship/youth-fellowship.component';
+import { ChildrenMinistryComponent } from './pages/ministry/children-ministry/children-ministry.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { TimeComponent } from './pages/time/time.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HttpClientModule } from '@angular/common/http';
+import "node_modules/@fullcalendar/common/main.css";
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { Appservice } from './app.service';
+import { FormsModule }   from '@angular/forms';
+
+FullCalendarModule.registerPlugins([
+  dayGridPlugin,
+  interactionPlugin
+]);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    MinistryComponent,
+    WomenFellowshipComponent,
+    YouthFellowshipComponent,
+    ChildrenMinistryComponent,
+    HistoryComponent,
+    TimeComponent,
+    GalleryComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FullCalendarModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [Appservice],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
